84de0c394fc82d9504a1ba4369e71c77c7a8ffc3
Jeremy Fox <jeremy.fox@cps.scot>
2019-11-25 16:02:03 +0000

    lots of changes

---
+++ PieShopTest/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PieShopTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShopTest.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+
+    }
+}
