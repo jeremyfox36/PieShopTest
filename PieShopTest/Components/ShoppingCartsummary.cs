0f8956c0dcf100323b198f47042cda242d75c75f
Jeremy Fox <jeremy.fox@cps.scot>
2019-12-12 15:44:25 +0000

    Amendedshopping cart and added view component and view model

--- PieShopTest/Components/ShoppingCartsummary.cs
+++ PieShopTest/Components/ShoppingCartsummary.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using PieShopTest.Models;
+using PieShopTest.ViewModels;
 
 namespace PieShopTest.Components
 {
-    public class ShoppingCartsummary : ViewComponent
+    public class ShoppingCartSummary : ViewComponent
     {
         private readonly ShoppingCart _shoppingCart;
-        public ShoppingCartsummary()
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
         {
+            _shoppingCart = shoppingCart;
+        }
 
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
         }
     }
 }
